2b69f28e33ad11c60dd5904262178d45ab3ee702
PatrickIOb <patrick.bauer95@gmx.net>
2025-11-24 13:03:39 +0100

    Implemented Data Structure and changed gitignore

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, jinja2, render_template
+import json
+import os
 
 app = Flask(__name__)
 
+DATA_FILE = os.path.join(os.path.dirname(__file__), 'data/storage.json')
+
+
+def load_blog_posts():
+    """Load blog posts from JSON storage file."""
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_blog_posts()
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
